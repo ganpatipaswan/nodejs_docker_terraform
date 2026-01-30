0bf65282e9859d5b73a949f8cff7b86740d1a7d1
ganpatipaswan <32195368+ganpatipaswan@users.noreply.github.com>
2026-01-30 05:58:19 +0530

    terraform and ansible deployemnt configurations

--- app/index.js
+++ app/index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 Node.js App Deployed using Docker & Terraform!--update--");
+  res.send("🚀 Node.js App Deployed using Docker & Terraform!--update-ansible- Node.js App Deployed using Docker + Ansible + EC2");
 });
+
+app.get("/test", (req, res) => {
+    res.json({
+        message: "Updated version deployed successfully ✅",
+        version: "v2 test"
+      });
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
